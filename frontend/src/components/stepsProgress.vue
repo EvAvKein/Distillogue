c9c65df6eb487eb0d0eb930536196c59d2bb3b4b
Eve Aviv Keinan <evavkein@gmail.com>
2022-02-16 09:31:53 +0200

    created stepsProgress component
    for use in upcoming feature

---
+++ frontend/src/components/stepsProgress.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="progressContainer">
+    <div v-for="number of totalSteps"
+      :class="number < currentStep ? 'completed' : ''"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    totalSteps:number,
+    currentStep:number,
+    emSize?:number,
+  }>();
+</script>
+
+<style scoped>
+  .progressContainer {
+    font-size: v-bind("(emSize || 1) + 'em'");
+    margin: auto;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    gap: 0.75em;
+  }
+
+  .progressContainer div {
+    opacity: 0.75;
+    height: 1em;
+    width: 1em;
+    border-radius: 2em;
+    background-color: var(--textSubColor);
+  }
+
+  div.completed {
+    opacity: 1;
+    background-color: var(--textColor);
+  };
+</style>
