a77b2aa1f57f2d34bc7b046933b3ae21646c0e65
Eve Aviv Keinan <evavkein@gmail.com>
2022-02-23 21:50:58 +0200

    formatting fixes in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,13 @@
-import {defineConfig} from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from "path";
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3030
+    port: 3030,
   },
   define: {
-    'process.env': {}
-  }
+    "process.env": {},
+  },
 });
