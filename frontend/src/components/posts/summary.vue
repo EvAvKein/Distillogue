fafdeaffda46db2b6c85579130b5add0a6bedf86
Eve Aviv Keinan <evavkein@gmail.com>
2022-02-27 20:45:41 +0200

    edited posts' summary.vue to accomodate prev commit
    tried to git-amend it into that prev commit but it didn't work :\

--- frontend/src/components/posts/summary.vue
+++ frontend/src/components/posts/summary.vue
@@ -3,7 +3,7 @@
     <h4>{{summary.title}}</h4>
     <h5><timestamp :pastUnix="summary.stats.lastActiveUnix"/></h5>
     <h6>Public: {{summary.public}}</h6>
-    <p>Replies: {{summary.stats.replyCount}}</p>
+    <p>Replies: {{summary.replyCount}}</p>
   </router-link>
 </template>
 
