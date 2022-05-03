0d62627808faea80b368d07cffdb364d78c1a108
Eve Aviv Keinan <evavkein@gmail.com>
2022-05-03 18:42:47 +0300

    added basic styling to config in post-creation

--- frontend/src/components/posts/create/config/editConfig.vue
+++ frontend/src/components/posts/create/config/editConfig.vue
@@ -1,51 +1,54 @@
 <template>
   <section>
     <details>
-    <summary>Voting</summary>
+      <summary>Voting</summary>
       <div>
-        Upvotes:
-        <input type="checkbox"
-          @change="(event) => {editConfig(event, 'votes', 'up')}"
-        />
-      </div>
-      <div>
-        Downvotes:
-        <input type="checkbox"
-          @change="(event) => {editConfig(event, 'votes', 'down')}"
-        />
-      </div>
-      <div>
-        Anonymous:
-        <input type="checkbox"
-          @change="(event) => {editConfig(event, 'votes', 'anon')}"
-        />
-      </div>
-    </details>
-    <details>
-    <summary>Activity</summary>
-      <div>
-        Last Active:
-        <input type="checkbox"
-          @change="(event) => {editConfig(event, 'lastActive')}"
-        />
+        <div>
+          Upvotes:
+          <input type="checkbox"
+            @change="(event) => {editConfig(event, 'votes', 'up')}"
+          />
+        </div>
+        <div>
+          Downvotes:
+          <input type="checkbox"
+            @change="(event) => {editConfig(event, 'votes', 'down')}"
+          />
+        </div>
+        <div>
+          Anonymous:
+          <input type="checkbox"
+            @change="(event) => {editConfig(event, 'votes', 'anon')}"
+          />
+        </div>
       </div>
     </details>
     <details>
-    <summary>Visibility</summary>
+      <summary>Activity</summary>
       <div>
-        Public:
-        <input type="checkbox"
-          @change="(event) => {editConfig(event, 'public')}"
-        />
+        <div>
+          Last Active:
+          <input type="checkbox"
+            @change="(event) => {editConfig(event, 'lastActive')}"
+          />
+        </div>
+      </div>
+    </details>
+    <details>
+      <summary>Visibility</summary>
+      <div>
+        <div>
+          Public:
+          <input type="checkbox"
+            @change="(event) => {editConfig(event, 'public')}"
+          />
+        </div>
       </div>
     </details>
   </section>
 </template>
 
 <script setup lang="ts">
-  // import {ref, computed} from "vue";
-  // import {useRouter} from "vue-router";
-  // import {useUser} from "../stores/user";
   import {PostConfig} from "../../../../../../backend/src/objects";
 
   const props = defineProps<{
@@ -71,5 +74,34 @@
 </script>
 
 <style scoped>
-  
+  section {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    gap: 0.75em;
+  }
+
+  details {
+    min-width: 15em;
+    max-width: 50%;
+    background-color: var(--backgroundSubColor);
+    border-radius: 0.5em;
+  }
+  details[open] {
+    flex-basis: 100%;
+    max-width: none;
+  }
+
+  summary {
+    font-size: 1.25em;
+    cursor: pointer;
+    padding: 0.25em;
+    padding-left: 0.5em;
+  }
+
+  details > div {
+    display: flex;
+    justify-content: space-around;
+    padding: 1em;
+  }
 </style>
