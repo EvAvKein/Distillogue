5b11c1aa365ea93ab38776886a89e631bf8a408f
Eve Aviv Keinan <evavkein@gmail.com>
2022-05-07 07:46:18 +0300

    added disclaimer/notice comment in lookupInOptional helper

--- backend/src/helpers/lookupInOptional.ts
+++ backend/src/helpers/lookupInOptional.ts
@@ -1,2 +1,4 @@
 export type lookupInOptional<T, K> = T extends undefined ? undefined : K extends keyof T ? T[K] : unknown;
-// see: https://stackoverflow.com/a/71097897/15002326
+// this is a temporary helper, because it enables overlooking data validation which should happen in runtime. once the project starts addressing data validation, this should be trashed (or maybe just kept for prototyping)
+
+// source: https://stackoverflow.com/a/71097897/15002326
