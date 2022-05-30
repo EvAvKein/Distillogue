8ff263abbf9dc7cb8823a1fd01e49bebf9e8111b
Eve Aviv Keinan <evavkein@gmail.com>
2022-05-30 23:04:08 +0300

    extracted dashboard's profile editor into its own component
    this was the only page which didn't have its section(s, but currently singular) properly modularized & imported

---
+++ frontend/src/components/users/profileEditor.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <textBox
+      :title="{
+        text: 'Name',
+        headingRank: 4,
+        emSize: 1
+      }"
+      :content="{
+        text: user.data.name,
+        singleLine: true,
+        editable: {
+          editAuthAddress: '/editProfileInfo',
+          dataName: 'name',
+        },
+        emSize: 1.5
+      }"
+    />
+    <textBox
+      :title="{
+        text: 'About',
+        headingRank: 4,
+        emSize: 1
+      }"
+      :content="{
+        text: user.data.about || '',
+        editable: {
+          editAuthAddress: '/editProfileInfo',
+          dataName: 'about',
+        },
+        emSize: 1.5
+      }"
+    />
+  </section>
+</template>
+
+<script setup lang="ts">
+  import {useUser} from "../../stores/user";
+  import textBox from '../../components/textBox.vue';
+  const user = useUser();
+</script>
+
+<style scoped>
+  * + * {margin-top: 1em;}
+</style>
--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,54 +1,18 @@
 <template>
   <main>
-    <textBox
-      :title="{
-        text: 'Name',
-        headingRank: 4,
-        emSize: 1
-      }"
-      :content="{
-        text: user.data.name,
-        singleLine: true,
-        editable: {
-          editAuthAddress: '/editProfileInfo',
-          dataName: 'name',
-        },
-        emSize: 1.5
-      }"
-    />
-    <textBox
-      :title="{
-        text: 'About',
-        headingRank: 4,
-        emSize: 1
-      }"
-      :content="{
-        text: user.data.about || '',
-        editable: {
-          editAuthAddress: '/editProfileInfo',
-          dataName: 'about',
-        },
-        emSize: 1.5
-      }"
-    />
+    <profileEditor id="profileEditor"/>
   </main>
 </template>
 
 <script setup lang="ts">
-  import {useUser} from "../stores/user";
-  import textBox from '../components/textBox.vue';
-  const user = useUser();
+  import profileEditor from '../components/users/profileEditor.vue';
 </script>
 
 <style scoped>
-  main {
-    padding: 1em;
-    display: flex;
-    flex-flow: column nowrap;
-    gap: 1em;
-  }
+  main {padding: 1em}
 
-  main > * {
-    width: clamp(15em, 90vw, 90em);
+  #profileEditor {
+    margin: 0 auto;
+    max-width: 50em;
   }
 </style>
