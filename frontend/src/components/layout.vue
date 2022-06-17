9e788b970dce38fc60842accfc287ccdc8aba910
Eve Aviv Keinan <evavkein@gmail.com>
2022-06-17 17:43:33 +0300

    style & refactor (header): icons, textless at low width, no frontpage link while logged

--- frontend/src/components/layout.vue
+++ frontend/src/components/layout.vue
@@ -2,23 +2,28 @@
   <header>
     <section id="leftSection">
       <nav>
-        <router-link
+        <div v-if="user.data.id">
+          <router-link v-if="user.data.id"
+            :to="{name: 'browse'}"
+            class="globalStyle_textButton"
+          >
+            <img src="../assets/browse.svg"/>
+            <span>Browse</span>
+          </router-link>
+          <router-link
+            :to="{name: 'createPost'}"
+            class="globalStyle_textButton"
+          >
+            <img src="../assets/post.svg"/>
+            <span>Post</span>
+          </router-link>
+        </div>
+        <router-link v-else
           :to="{name: 'home'}"
+          aria-label="Front page"
           id="logoLink"
         >
           <img src="../assets/logo.svg"/>
-        </router-link>
-        <router-link v-if="user.data.id"
-          :to="{name: 'browse'}"
-          class="globalStyle_textButton"
-        >
-          Browse
-        </router-link>
-        <router-link v-if="user.data.id"
-          :to="{name: 'createPost'}"
-          class="globalStyle_textButton"
-        >
-          Post
         </router-link>
       </nav>
     </section>
@@ -30,19 +35,22 @@
             :to="{name: 'dashboard'}"
             class="globalStyle_textButton"
           >
-            Dashboard
+            <img src="../assets/dashboard.svg"/>
+            <span>Dashboard</span>
           </router-link>
           <button @click="logOut"
             class="globalStyle_textButton"
           >
-            Logout
+            <img src="../assets/leave.svg"/>
+            <span>Logout</span>
           </button>
         </div>
         <router-link v-else
           :to="{name: 'join'}"
           class="globalStyle_textButton"
         >
-          Join
+          <img src="../assets/userWithoutPfp.svg"/>
+          <span>Join</span>
         </router-link>
       </nav>
     </section>  
@@ -70,19 +78,20 @@
     width: 100%;
     height: 4em;
     z-index: 900;
+    white-space: nowrap;
     background-color: var(--backgroundColor);
     display: flex;
     justify-content: space-between;
   }
 
-  #logoLink {height: 2.75em} /* without explictly declaring this, the button's height is annoying taller than its content for some unknown reason (at least on chrome 99) */ 
+  #logoLink {height: 2.75em} /* without explictly declaring this, the button is annoying higher (not taller) than its box-shadow (at least on chrome 99) */ 
+  #logoLink img {height: 2.75em}
   #logoLink:focus, #logoLink:hover {
     outline: none;
     border-radius: 10em;
     box-shadow: 0em 0em 0.35em 0.35em var(--highlightSubColor);
   }
-  #logoLink:active {box-shadow: 0em 0em 0.35em 0.35em var(--highlightColor);}
-  #logoLink img {height: 2.75em}
+  #logoLink:active {box-shadow: 0em 0em 0.35em 0.35em var(--highlightColor)}
 
   nav, nav > div {
     height: 3em;
@@ -90,6 +99,24 @@
     width: fit-content;
     display: flex;
     align-items: center;
-    gap: 0.5em
+  }
+
+  .globalStyle_textButton {font-size: 0.9em}
+
+  img:not(#logoLink img) {
+    height: 1.25em;
+    vertical-align: middle;
+    filter: var(--filterToBackgroundColor);
+  }
+
+  span {
+    font-size: 1.1em;
+    vertical-align: top;
+    margin-left: 0.25em;
+    display: none;
+  }
+
+  @media (min-width: 32rem) {
+    span {display: inline}
   }
 </style>
