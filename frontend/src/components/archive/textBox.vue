4b31325f976cb6b7e0314b380be4c58ecc92ee34
Eve Aviv Keinan <evavkein@gmail.com>
2022-06-19 16:27:40 +0300

    chore: move unused components to archive

--- frontend/src/components/archive/textBox.vue
+++ frontend/src/components/archive/textBox.vue
@@ -47,9 +47,9 @@
 
 <script setup lang="ts">
   import {ref} from "vue";
-  import {useUser} from "../stores/user";
-  import {jsonFetch} from "../helpers/jsonFetch";
-  import editNotification from "./notification.vue";
+  import {useUser} from "../../stores/user";
+  import {jsonFetch} from "../../helpers/jsonFetch";
+  import editNotification from "../notification.vue";
   const user = useUser();
 
   const props = defineProps<{
