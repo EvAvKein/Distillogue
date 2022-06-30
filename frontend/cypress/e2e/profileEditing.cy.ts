a048e7a6c3a81278141b435cf5024c938344a36a
Eve Aviv Keinan <evavkein@gmail.com>
2022-06-30 21:24:08 +0300

    test (profileEditing): add testing for drafts section

--- frontend/cypress/e2e/profileEditing.cy.ts
+++ frontend/cypress/e2e/profileEditing.cy.ts
@@ -73,3 +73,98 @@ describe("Profile editing", () => {
     validateProfileData();
   });
 });
+
+describe("Drafts editing", () => {
+  it("Switch to Drafts subpage", () => {
+    cy.contains("button", "Drafts").click();
+
+    cy.should("not.contain.text", "Title");
+    cy.should("not.contain.text", "Body");
+    cy.should("not.contain.text", "Save");
+    cy.should("not.contain.text", "Delete");
+  });
+
+  function createAndValidateDraft(title:string, body:string) {
+    cy.contains("button", "New Draft").click();
+    cy.contains("button", "[No Title]").should("contain.text", "Edited: Now");
+    cy.contains("button", "Delete");
+    cy.contains("button", "Save");
+
+    cy.contains("label", "Title").type(title);
+    cy.contains("button", title);
+    cy.contains("label", "Body").type(body);
+    cy.get("main").contains("Save").click();
+    cy.wait(250);
+    cy.get(".notification.positive").should("contain.text", "Changes saved!");
+
+    cy.reload();
+    cy.contains("button", "Drafts").click();
+    cy.contains("button", title).click();
+    cy.contains("label", "Title").click().focused().should("have.value", title);
+    cy.contains("label", "Body").click().focused().should("have.value", body);
+  };
+
+  let title = "Draft Title By Cypress!";
+  let body = "Draft Body By Cypress!\n\nThis is a bit of filler text because the body is supposed to be longer :P";
+  it("Create, write, and save draft", () => {
+    createAndValidateDraft(title, body);
+  });
+
+  it("Edit existing draft", () => {
+    title = "Different " + title;
+    body = "Different " + body;
+    
+    cy.contains("button", "Drafts").click();
+
+    cy.contains("label", "Title").click().focused().clear().type(title);
+    cy.contains("button", title);
+    cy.contains("button", "Save");
+
+    cy.contains("label", "Body").click().focused().clear().type(body);
+    cy.contains("button", "Save").click();
+    cy.wait(250);
+    cy.get(".notification.positive").should("contain.text", "Changes saved!");
+
+    cy.reload();
+    cy.contains("button", "Drafts").click();
+    cy.contains("button", title).click();
+    cy.contains("label", "Title").click().focused().should("have.value", title);
+    cy.contains("label", "Body").click().focused().should("have.value", body);
+  });
+
+  it("Create drafts to capacity", () => {
+    createAndValidateDraft("Second " + title, "Second " + body);
+    createAndValidateDraft("Third " + title, "Third " + body);
+    cy.should("not.contain.text", "New Draft");
+    cy.get(".notification").should("contain.text", "Drafts at capacity, consider triage");
+  });
+
+  it("Delete drafts", () => {
+    cy.contains("button", "Second " + title).click();
+    cy.contains("button", "Delete").click();
+    cy.get("main").should("not.contain.text", "Second " + title);
+    cy.contains("button", "Save").click();
+    cy.wait(250);
+    cy.get(".notification.positive").should("contain.text", "Changes saved!");
+
+    cy.reload();
+    cy.contains("button", "Drafts").click();
+    cy.contains("button", title);
+    cy.should("not.contain.text", "Second " + title);
+    cy.contains("button", "Third " + title);
+
+    cy.contains("button", "Third " + title).click();
+    cy.contains("button", "Delete").click();
+    cy.get("main").should("not.contain.text", "Third " + title);
+    cy.contains("button", title).click();
+    cy.contains("Delete").click();
+    cy.get("main").should("not.contain.text", title);
+    cy.contains("button", "Save").click();
+    cy.wait(250);
+    cy.get(".notification.positive").should("contain.text", "Changes saved!");
+
+    cy.reload();
+    cy.contains("button", "Drafts").click();
+    cy.should("not.contain.text", title);
+  });
+});
