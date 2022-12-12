940ce3e1d617c0d64d4edcbd49d87b6ef51f5e6b
Eve_Aviv <evavkein@gmail.com>
2022-12-12 07:07:56 +0200

    refactor (mongo module): fix type in database name

--- backend/src/mongo.ts
+++ backend/src/mongo.ts
@@ -4,7 +4,7 @@ import {MongoClient, Collection} from "mongodb";
 const mongo = new MongoClient(process.env.DOCKERIZED ? "mongodb://mongodb:27017" : "mongodb://localhost");
 await mongo.connect().catch(() => {console.log("Failed to connect to database :\\")});
 
-const database = mongo.db("distllogue");
+const database = mongo.db("distillogue");
 const users = database.collection("users") as Collection<User>;
 const posts = database.collection("posts") as Collection<Node>;
 
