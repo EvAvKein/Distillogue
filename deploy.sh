94426a45c1c7e952bc4399410abbe94c74716a73
Eve_Aviv <evavkein@gmail.com>
2023-01-03 19:44:56 +0200

    fix (deploy.sh): add missing command/suffix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker-compose up --detach &&
-docker-compose -f docker-compose.ssl.yaml &&
+docker-compose -f docker-compose.ssl.yaml up &&
 docker-compose down &&
 docker-compose -f docker-compose.yaml -f docker-compose.httpsOverride.yaml up
