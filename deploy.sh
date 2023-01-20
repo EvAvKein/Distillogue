ed5ac9164e1d42cf1a452d0a4020d9acb6000224
Eve_Aviv <evavkein@gmail.com>
2023-01-20 07:38:36 +0200

    fix (deploy.sh): remove unneeded dev stage
    bit ashamed that i only just figured out the problem with this (presumably, still need to test), extremely simple obvious in hindsight

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
-docker-compose up --detach &&
-docker-compose -f docker-compose.ssl.yaml up &&
-docker-compose down &&
-docker-compose -f docker-compose.yaml -f docker-compose.httpsOverride.yaml up
+docker-compose -f docker-compose.yaml -f docker-compose.httpsOverride.yaml up --detach && 
+docker-compose -f docker-compose.ssl.yaml up
