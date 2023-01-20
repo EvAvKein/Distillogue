6d930a416caf99a0c542423acfebcbecd636be97
Eve_Aviv <evavkein@gmail.com>
2023-01-20 08:12:18 +0200

    revert (deployment): restore acme-challenge in dev.nginx & restore dev stage in deploy.sh
    thought i figured out the issue, testing it revealed that there's a reason this solution didn't work in the first place. i think i might've solved the real issue with a recent commit (adding acme-challenge & letsEncrypt volumes to nginx)

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
-docker-compose -f docker-compose.yaml -f docker-compose.httpsOverride.yaml up --detach && 
-docker-compose -f docker-compose.ssl.yaml up
+docker-compose up --detach &&
+docker-compose -f docker-compose.ssl.yaml up &&
+docker-compose down &&
+docker-compose -f docker-compose.yaml -f docker-compose.httpsOverride.yaml up
