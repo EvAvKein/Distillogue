846c5809d2dc9b7cf25c492a3bdcf2f7f7dfe5b7
Eve_Aviv <evavkein@gmail.com>
2023-02-03 23:02:39 +0200

    refactor (dashboard.vue): remove edits processing
    the final commit on the matter after refactoring all current dashboard pages to auto-save and handle API requests within themselves. made this series of changes for both UX and ease of testing

--- frontend/src/components/users/dashboard.vue
+++ frontend/src/components/users/dashboard.vue
@@ -1,112 +1,44 @@
 <template>
 	<section id="dashboardContainer">
-		<section id="dashboardSidebar">
-			<transition name="collapse">
-				<button
-					v-show="changes.length > 0"
-					id="dashboardSubmit"
-					class="core_backgroundButton"
-					@click="submitAllChanges"
-				>
-					<img src="../../assets/save.svg" alt="Floppy disk icon" />
-					<span>Save</span>
-				</button>
-			</transition>
-			<nav aria-label="Dashboard page navigation">
-				<button @click="currentPage = 'profile'" :inert="currentPage === 'profile'" class="core_contentButton">
-					<img src="../../assets/userWithoutPfp.svg" alt="Non-descript person icon" />
-					<span>Profile</span>
-				</button>
-				<button @click="currentPage = 'drafts'" :inert="currentPage === 'drafts'" class="core_contentButton">
-					<img src="../../assets/drafts.svg" alt="Paper tray icon" />
-					<span>Drafts</span>
-				</button>
-				<button @click="currentPage = 'presets'" :inert="currentPage === 'presets'" class="core_contentButton">
-					<img
-						src="../../assets/presets.svg"
-						alt="Icon of a browser window stacked on another one, with the top window containing a cogwheel"
-					/>
-					<span>Presets</span>
-				</button>
-				<button @click="currentPage = 'contacts'" :inert="currentPage === 'contacts'" class="core_contentButton">
-					<img src="../../assets/contacts.svg" alt="Address book icon" />
-					<span>Contacts</span>
-				</button>
-			</nav>
-		</section>
-		<section id="dashboardPage">
-			<notification v-model:text="submitNotif.text" :desirablityStyle="submitNotif.style" />
-			<profileEditor v-if="currentPage === 'profile'" @newState="updateChangesByNewState" />
-			<draftsEditor v-if="currentPage === 'drafts'" @newState="updateChangesByNewState" />
-			<presetsEditor v-if="currentPage === 'presets'" @newState="updateChangesByNewState" />
-			<contactsEditor v-if="currentPage === 'contacts'" @newState="updateChangesByNewState" />
-		</section>
+		<nav aria-label="Dashboard navigation">
+			<button @click="currentPage = 'profile'" :inert="currentPage === 'profile'" class="core_contentButton">
+				<img src="../../assets/userWithoutPfp.svg" alt="Non-descript person icon" />
+				<span>Profile</span>
+			</button>
+			<button @click="currentPage = 'drafts'" :inert="currentPage === 'drafts'" class="core_contentButton">
+				<img src="../../assets/drafts.svg" alt="Paper tray icon" />
+				<span>Drafts</span>
+			</button>
+			<button @click="currentPage = 'presets'" :inert="currentPage === 'presets'" class="core_contentButton">
+				<img
+					src="../../assets/presets.svg"
+					alt="Icon of a browser window stacked on another one, with the top window containing a cogwheel"
+				/>
+				<span>Presets</span>
+			</button>
+			<button @click="currentPage = 'contacts'" :inert="currentPage === 'contacts'" class="core_contentButton">
+				<img src="../../assets/contacts.svg" alt="Address book icon" />
+				<span>Contacts</span>
+			</button>
+		</nav>
+	</section>
+	<section id="dashboardPage">
+		<profileEditor v-if="currentPage === 'profile'" />
+		<draftsEditor v-if="currentPage === 'drafts'" />
+		<presetsEditor v-if="currentPage === 'presets'" />
+		<contactsEditor v-if="currentPage === 'contacts'" />
 	</section>
 </template>
 
 <script setup lang="ts">
-	import {ref, reactive} from "vue";
-	import {useUser} from "../../stores/user";
-	import {useDashboardEdits} from "../../stores/dashboardEdits";
-	import {apiFetch} from "../../helpers/apiFetch";
-	import {UserPatchRequest} from "../../../../shared/objects/api";
-	import {editableUserData} from "../../../../shared/objects/user";
-	import {deepCloneFromReactive} from "../../helpers/deepCloneFromReactive";
-	import notification from "../notification.vue";
+	import {ref} from "vue";
 	import profileEditor from "./dashboardSections/profileEditor.vue";
 	import draftsEditor from "./dashboardSections/draftsEditor.vue";
 	import presetsEditor from "./dashboardSections/presetsEditor.vue";
 	import contactsEditor from "./dashboardSections/contactsEditor.vue";
-	const user = useUser();
-	const changes = useDashboardEdits().edits;
 
 	type pageName = "profile" | "drafts" | "presets" | "contacts";
 	const currentPage = ref<pageName>("profile");
-	const submitNotif = reactive({text: "", style: null as boolean | null});
-
-	function updateChangesByNewState(newStates: UserPatchRequest<editableUserData>[]) {
-		newStates.forEach((state) => {
-			const existingChangeIndex = changes.findIndex((change) => change.dataName === state.dataName);
-			const prevChangeExists = existingChangeIndex === -1 ? false : true;
-
-			const inputMatchesUserData =
-				typeof state.newValue === "object" && state.newValue !== null
-					? JSON.stringify(state.newValue) === JSON.stringify(user.data![state.dataName]) // flawed (as it depends on the contents of both to be in the same sequence), but it's *good enough* as of the current stage of dev (june 2022)
-					: state.newValue === user.data![state.dataName];
-			if (!prevChangeExists && inputMatchesUserData) {
-				return;
-			}
-
-			if (!prevChangeExists) {
-				changes.push(state);
-				return;
-			}
-
-			inputMatchesUserData
-				? changes.splice(existingChangeIndex, 1)
-				: (changes[existingChangeIndex].newValue = state.newValue);
-		});
-	}
-
-	async function submitAllChanges() {
-		submitNotif.text = "Submitting changes...";
-		submitNotif.style = null;
-
-		const changesResponse = await apiFetch("PATCH", "/users", changes);
-
-		if (changesResponse.error) {
-			submitNotif.text = changesResponse.error.message;
-			submitNotif.style = false;
-			return;
-		}
-
-		submitNotif.text = "Changes saved!";
-		submitNotif.style = true;
-		changes.forEach((change) => {
-			(user.data![change.dataName] as any) = deepCloneFromReactive(change).newValue; // considering the circumstances, i dont think coercing an "any" here is actually harmful
-		});
-		changes.length = 0;
-	}
 </script>
 
 <style scoped>
@@ -116,15 +48,11 @@
 		gap: 0.75em;
 	}
 
-	#dashboardSidebar button {
+	button {
 		width: 100%;
 		white-space: nowrap;
 		font-weight: bold;
 		padding: 0.4em;
-	}
-
-	#dashboardSubmit img {
-		filter: var(--filterToBackgroundColor);
 	}
 
 	img {
