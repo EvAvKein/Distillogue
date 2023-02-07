f954533273db42cf89d98098592f213add64992a
Eve_Aviv <evavkein@gmail.com>
2023-02-07 17:05:26 +0200

    refactor (mongo.ts): type DBs by generic instead of type override

--- backend/src/mongo.ts
+++ backend/src/mongo.ts
@@ -1,11 +1,11 @@
 import {User} from "../../shared/objects/user";
 import {Node} from "../../shared/objects/post";
-import {MongoClient, Collection} from "mongodb";
+import {MongoClient} from "mongodb";
 const mongo = new MongoClient(process.env.DOCKERIZED ? "mongodb://mongodb:27017" : "mongodb://localhost");
 await mongo.connect().catch(() => console.log("Failed to connect to database :\\"));
 
 const database = mongo.db("distillogue");
-const users = database.collection("users") as Collection<User>;
-const posts = database.collection("posts") as Collection<Node>;
+const users = database.collection<User>("users");
+const posts = database.collection<Node>("posts");
 
 export {users, posts};
