1cdd05f779729564c2ad88d5a4e54921aa2a0c94
Eve_Aviv <evavkein@gmail.com>
2023-02-09 16:35:34 +0200

    test (helpers): create requestsByApi.ts
    because:
    1. API tests
    2. most test will need some setup, and contacting the API more performant than using an interface

---
+++ testing/helpers/requestsByApi.ts
@@ -0,0 +1,36 @@
+import {type PlaywrightTestArgs, type APIRequestContext, expect} from "@playwright/test";
+import {randomUsername} from "./randomAlphanumString.js";
+type request = PlaywrightTestArgs["request"];
+
+async function createUser(request: request, name?: string) {
+	return request.post("/api/users", {data: {username: name ?? randomUsername()}});
+}
+async function createSession(request: request, name: string) {
+	return request.post("/api/sessions", {data: {username: name}});
+}
+async function getSession(request: request, authKey: string) {
+	return request.get("/api/sessions", {
+		headers: {authorization: "Bearer" + authKey},
+	});
+}
+async function deleteSession(request: request, authKey: string) {
+	return request.delete("/api/sessions", {
+		headers: {authorization: "Bearer" + authKey},
+	});
+}
+
+async function createUserAndSession(request: APIRequestContext) {
+	const name = randomUsername();
+
+	const userCreationResponse = await createUser(request, name);
+	expect(userCreationResponse.ok()).toBeTruthy();
+
+	const sessionCreationResponse = await createSession(request, name);
+	expect(sessionCreationResponse.ok()).toBeTruthy();
+	const sessionKey = (await sessionCreationResponse.json())?.data?.sessionKey;
+	expect(typeof sessionKey).toBe("string");
+
+	return {name: name, sessionKey: sessionKey as string};
+}
+
+export {createUser, createSession, getSession, deleteSession, createUserAndSession};
