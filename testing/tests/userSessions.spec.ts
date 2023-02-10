918c7d1c2d07d98e72d1abf1a12707677ad37227
Eve_Aviv <evavkein@gmail.com>
2023-02-10 16:21:38 +0200

    test (userSessions): rewrite for playwright
    previously it was just a direct syntax translation of the previous Cypress test, now it's actually functioning

--- testing/tests/userSessions.spec.ts
+++ testing/tests/userSessions.spec.ts
@@ -1,44 +1,67 @@
 import {test, expect} from "@playwright/test";
-import {signIn, signUp} from "../helpers/requestsByUi.js";
-import {randomUsername} from "../helpers/randomAlphanumString.js";
+import {createSession, getSession, deleteSession, createUserAndSession} from "../helpers/requestsByApi.js";
+import {getSessionKey, setSessionKey} from "../helpers/sessionKey.js";
+import {signUp} from "../helpers/requestsByUi.js";
 
-test.describe.fixme("User session CRUD", () => {
-	let username = "Session" + randomUsername();
-	let sessionKey: string;
+test.describe("User sessions - API", async () => {
+	test("Fail to reuse key after session deletion", async ({request}) => {
+		const {sessionKey} = await createUserAndSession(request);
 
-	test("Sign up & navigate to session-required page", async ({page}) => {
-		await signUp(page, username);
-		sessionKey = window.localStorage.getItem("sessionKey") || "";
+		await expect(await deleteSession(request, sessionKey)).toBeOK();
 
-		await page.locator("header").getByText("Post").click();
-
-		await expect(page).toHaveURL(/.*post\/create/);
+		const invalidSessionRequest = await getSession(request, sessionKey);
+		expect((await invalidSessionRequest.json()).data).toBeNull();
 	});
 
-	test("Sign out & fail to access session-required page", async ({page}) => {
+	test("Obtain different key for new session by same user", async ({request}) => {
+		const {name, sessionKey: firstKey} = await createUserAndSession(request);
+
+		await expect(await deleteSession(request, firstKey)).toBeOK();
+
+		const secondSessionResponse = await createSession(request, name);
+		await expect(secondSessionResponse).toBeOK();
+		const secondKey = (await secondSessionResponse.json())?.data?.sessionKey;
+		expect(typeof secondKey).toBe("string");
+		expect(secondKey as string).not.toEqual(firstKey);
+	});
+
+	// test("get sessions list & update (TODO pending endpoint, which is pending dashboard section)", async () => {});
+});
+
+test.describe("User sessions - UI", async () => {
+	test("Delete session key on logout", async ({page}) => {
+		await signUp(page);
+		expect(typeof (await getSessionKey(page))).toBe("string");
+
 		await page.locator("header").getByText("Logout").click();
-		await expect(page).toHaveURL(/.*join/);
-		expect(window.localStorage.getItem("sessionKey")).toBeFalsy();
-
-		await page.goto("/post/create");
-		await expect(page).not.toHaveURL(/.*post\/create/);
-		await expect(page).toHaveURL(/.*join/);
+		expect(await getSessionKey(page)).toBeNull();
 	});
 
-	test("Fail to reuse deleted session by inserting key to localStorage", async ({page}) => {
-		window.localStorage.setItem("sessionKey", sessionKey || "");
+	const sessionRequiredPage: string[] = ["/dashboard", "/post/create"];
 
-		await page.goto("/post/create");
-		await expect(page).not.toHaveURL(/.*post\/create/);
-		await expect(page).toHaveURL(/.*join/);
+	test("Fail to navigate to any session-required page while not signed in", async ({page}) => {
+		for (const pageURL of sessionRequiredPage) {
+			await page.goto(pageURL);
+			await expect(page).toHaveURL(/.*join/);
+		}
+	});
+	test("Navigate to all session-required pages while signed in", async ({page}) => {
+		await signUp(page);
+		for (const pageURL of sessionRequiredPage) {
+			await page.goto(pageURL);
+			await expect(page).toHaveURL(pageURL);
+		}
 	});
 
-	test("Sign in & obtain new session key", async ({page}) => {
-		await signIn(page, username);
+	test("Fail to reuse session key after logout", async ({page}) => {
+		await signUp(page);
+		const sessionKey = await getSessionKey(page);
+		expect(typeof sessionKey).toBe("string");
 
-		const newKey = window.localStorage.getItem("sessionKey");
-		expect(newKey).toBeTruthy();
-		expect(newKey).not.toEqual(sessionKey);
+		await page.locator("header").getByText("Logout").click();
+		await setSessionKey(page, sessionKey as string);
+		await page.goto("/post/create");
+		await expect(page).toHaveURL(/.*join/);
 	});
 
 	// test("View & update session in dashboard (TODO pending dashboard section)", async () => {});
