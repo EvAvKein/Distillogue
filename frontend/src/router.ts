88b6be82e83b9da72827d3d6f60b6b733c227ec6
Eve_Aviv <evavkein@gmail.com>
2023-02-24 01:28:28 +0200

    refactor (frontend router): minor improvements
    i created this module when i had half as much experience (and only a third-ish as much if we're just talking about javascript), so now that i'm revisiting it (in prep for dashboard subroutes) i've unsurprisingly discovered a few things due for a refactor

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -3,76 +3,74 @@ import {useUser} from "./stores/user";
 import {apiFetch} from "./helpers/apiFetch";
 import {UserData} from "../../shared/objects/user";
 
+import notFound from "./pages/NotFound.vue";
 import home from "./pages/Home.vue";
 import about from "./pages/About.vue";
 import terms from "./pages/Terms.vue";
+import join from "./pages/Join.vue";
 import browse from "./pages/Browse.vue";
 import createPost from "./pages/CreatePost.vue";
 import viewPost from "./pages/ViewPost.vue";
 import dashboard from "./pages/Dashboard.vue";
-import join from "./pages/Join.vue";
-import notFound from "./pages/NotFound.vue";
-
-const routes = [
-	{
-		name: "home",
-		path: "/",
-		component: home,
-	},
-	{
-		name: "about",
-		path: "/about",
-		component: about,
-	},
-	{
-		name: "terms",
-		path: "/terms",
-		component: terms,
-	},
-	{
-		name: "browse",
-		path: "/browse",
-		component: browse,
-		meta: {
-			accountRequired: true,
-		},
-	},
-	{
-		name: "createPost",
-		path: "/post/create",
-		component: createPost,
-		meta: {
-			accountRequired: true,
-		},
-	},
-	{
-		name: "viewPost",
-		path: "/post/:postId",
-		component: viewPost,
-		props: true,
-	},
-	{
-		name: "dashboard",
-		path: "/dashboard",
-		component: dashboard,
-		meta: {
-			accountRequired: true,
-		},
-	},
-	{
-		name: "join",
-		path: "/join",
-		component: join,
-	},
-	{
-		path: "/:pathMatch(.*)*",
-		component: notFound,
-	},
-];
 
 const router = createRouter({
 	history: createWebHistory(process.env.BASE_URL),
-	routes,
+	routes: [
+		{
+			name: "home",
+			path: "/",
+			component: home,
+		},
+		{
+			name: "about",
+			path: "/about",
+			component: about,
+		},
+		{
+			name: "terms",
+			path: "/terms",
+			component: terms,
+		},
+		{
+			name: "join",
+			path: "/join",
+			component: join,
+		},
+		{
+			name: "browse",
+			path: "/browse",
+			component: browse,
+			meta: {
+				accountRequired: true,
+			},
+		},
+		{
+			name: "createPost",
+			path: "/post/create",
+			component: createPost,
+			meta: {
+				accountRequired: true,
+			},
+		},
+		{
+			name: "viewPost",
+			path: "/post/:postId",
+			component: viewPost,
+			props: true,
+		},
+		{
+			name: "dashboard",
+			path: "/dashboard",
+			component: dashboard,
+			meta: {
+				accountRequired: true,
+			},
+		},
+		{
+			path: "/:pathMatch(.*)*",
+			component: notFound,
+		},
+	],
 });
 
 router.beforeEach(async (to, from, next) => {
@@ -86,17 +84,15 @@ router.beforeEach(async (to, from, next) => {
 				return;
 			}
 
-			if (status && status >= 400 && status < 500) localStorage.removeItem("sessionKey");
+			if (status && status >= 400 && status < 500) {
+				localStorage.removeItem("sessionKey");
+				user.$reset();
+			}
 		});
 	}
 
 	const accountRequired = to.matched.some((record) => record.meta.accountRequired);
-	if (!accountRequired) {
-		next();
-		return;
-	}
-
-	if (!user.data) {
+	if (accountRequired && !user.data) {
 		next({path: "/join"});
 		return;
 	}
