2cd6719cc22d6460b305ae1bfca5f443fa7bc232
Eve_Aviv <evavkein@gmail.com>
2023-03-07 17:11:49 +0200

    refactor (deploy.sh): deploy in detached console mode
    to make it easier to redeploy, and be able to do stuff without shutting down the server throughout (e.g git pull, experimental file edits that cant be tested locally)

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ docker system prune -a -f &&
 docker-compose up --detach &&
 docker-compose -f docker-compose.ssl.yaml up &&
 docker-compose down &&
-docker-compose -f docker-compose.yaml -f docker-compose.httpsOverride.yaml up
+docker-compose -f docker-compose.yaml -f docker-compose.httpsOverride.yaml up --detach
