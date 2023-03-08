160c7bd03304814190235ec9dc76509be66e2fd5
Eve_Aviv <evavkein@gmail.com>
2023-03-08 13:18:54 +0200

    refactor (timestamp.vue): simplify & improve readability

--- frontend/src/components/timestamp.vue
+++ frontend/src/components/timestamp.vue
@@ -1,54 +1,41 @@
 <template>
 	<span
-		:title="stampText === absoluteTimestamp ? '' : absoluteTimestamp"
-		:style="stampText === absoluteTimestamp ? {} : {cursor: 'help'}"
+		:title="timestamp === absoluteTimestamp ? '' : absoluteTimestamp"
+		:style="timestamp === absoluteTimestamp ? {} : {cursor: 'help'}"
 	>
-		{{ stampText }}
+		{{ timestamp }}
 	</span>
 </template>
 
 <script setup lang="ts">
+	const props = defineProps<{pastUnix: number}>();
+
 	const minute = 60;
 	const hour = minute * 60;
 	const day = hour * 24;
 	const week = day * 7;
-	function currentUnix() {
-		return Math.floor(Date.now() / 1000);
-	}
 
-	const props = defineProps<{pastUnix: number}>();
-	const dateObject = new Date(props.pastUnix * 1000);
-	function doubleDigiter(number: string | number) {
+	function dblDigit(number: string | number) {
 		return number < 10 ? "0" + number : number;
 	}
-	const absoluteTimestamp = `${doubleDigiter(dateObject.getHours())}:${doubleDigiter(
-		dateObject.getMinutes()
-	)}, ${doubleDigiter(dateObject.getDate())}.${doubleDigiter(dateObject.getMonth() + 1)}.${dateObject
-		.getFullYear()
-		.toString()
-		.slice(-2)}`;
 
-	function timestamp(pastUnix: number) {
-		const unixDiff = currentUnix() - pastUnix;
-		if (unixDiff > week) {
-			return absoluteTimestamp;
-		}
-		if (unixDiff > day) {
-			return Math.floor(unixDiff / day) + "d ago";
-		}
-		if (unixDiff > hour) {
-			return Math.floor(unixDiff / hour) + "h ago";
-		}
-		if (unixDiff > minute) {
-			return Math.floor(unixDiff / minute) + "m ago";
-		}
-		if (unixDiff > 15) {
-			return unixDiff + "s ago";
-		}
+	const da = new Date(props.pastUnix * 1000);
+	const absoluteTimestamp =
+		`${dblDigit(da.getHours())}:${dblDigit(da.getMinutes())}, ` +
+		`${dblDigit(da.getDate())}.${dblDigit(da.getMonth() + 1)}.${da.getFullYear().toString().slice(-2)}`;
+
+	function calcTimestamp() {
+		const currentUnix = Math.floor(Date.now() / 1000);
+		const unixDiff = currentUnix - props.pastUnix;
+		if (unixDiff > week) return absoluteTimestamp;
+		if (unixDiff > day) return Math.floor(unixDiff / day) + "d ago";
+		if (unixDiff > hour) return Math.floor(unixDiff / hour) + "h ago";
+		if (unixDiff > minute) return Math.floor(unixDiff / minute) + "m ago";
+		if (unixDiff > 15) return unixDiff + "s ago";
 		return "Now";
 	}
 
-	const stampText = props.pastUnix !== undefined ? timestamp(props.pastUnix) : "error: add pastUnix timestamp prop";
+	const timestamp = calcTimestamp();
 </script>
 
 <style scoped>
