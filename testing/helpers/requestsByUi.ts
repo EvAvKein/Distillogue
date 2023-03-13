f92449873ad8d0978066fb3400711dd9ebbcf8e3
Eve_Aviv <evavkein@gmail.com>
2023-03-13 13:10:35 +0200

    test (helpers - requestsByUi): remove outdated variable name & declare directly at usage
    the variable name "closedCategories" is no longer accurate since the selector was changed, and just naming it "categories" or "categoryElems" is kinda redundant considering the readability of the value declaration and its one-time usage

--- testing/helpers/requestsByUi.ts
+++ testing/helpers/requestsByUi.ts
@@ -55,9 +55,7 @@ async function createReply(page: Page, nodeTitlesPath: string[], replyTitle: str
 }
 
 async function expandConfigCategories(page: Page) {
-	const closedCategories = await page.locator(".configCategory").all();
-
-	for (const category of closedCategories) {
+	for (const category of await page.locator(".configCategory").all()) {
 		if ((await category.getAttribute("aria-label")) === "Closed category") {
 			await category.locator("> button").click();
 		}
