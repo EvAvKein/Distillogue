6cd8341709bf6a266cb6e888bd2667ae5293dcda
Eve_Aviv <evavkein@gmail.com>
2023-03-21 19:27:07 +0200

    fix (deploy.sh): attempt to fix container networking upon rebuilds
    can't debug this locally, so here's me attempting vague notions of possible fixes via git :))

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker-compose down &&
-docker-compose up --detach --build &&
-docker-compose -f docker-compose.ssl.yaml up --build &&
+docker-compose up --detach --build --force-recreate &&
+docker-compose -f docker-compose.ssl.yaml up --build --force-recreate &&
 docker-compose down &&
-docker-compose -f docker-compose.yaml -f docker-compose.httpsOverride.yaml up --build
+docker-compose -f docker-compose.yaml -f docker-compose.httpsOverride.yaml up --build --force-recreate
