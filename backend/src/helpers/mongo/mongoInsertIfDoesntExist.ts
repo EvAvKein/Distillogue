89f6e909e79dcb75eacb0b95d4a220c54fc6c58d
Eve_Aviv <evavkein@gmail.com>
2023-03-30 09:16:44 +0300

    fix (backend - helpers): 2nd half of mongoInsertIfDoesntExist.ts type-related fixes
    first commit was incomplete, mislabelled, and vaguely-titled all due to programming while tired...

--- backend/src/helpers/mongo/mongoInsertIfDoesntExist.ts
+++ backend/src/helpers/mongo/mongoInsertIfDoesntExist.ts
@@ -1,11 +1,11 @@
-import {Collection, Document} from "mongodb";
-import {DeepPartial} from "../deepPartial";
+import type {Collection, Document, UpdateOptions} from "mongodb";
+import type {DeepPartial} from "../deepPartial";
 
 export function mongoInsertIfDoesntExist<dbT extends Document>(
 	collection: Collection<dbT>,
 	object: dbT,
 	existenceFilter: DeepPartial<dbT>,
-	extraUpdateOptions?: {}
+	extraUpdateOptions?: UpdateOptions
 ) {
 	return collection.updateOne(existenceFilter, {$setOnInsert: object}, {upsert: true, ...extraUpdateOptions});
 }
