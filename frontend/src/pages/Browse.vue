50927b09e480ff4e6be1aa9c5a94a41679603a59
Eve_Aviv <evavkein@gmail.com>
2023-04-13 10:03:43 +0300

    refactor: delete summariesBrowser.vue & dashboard.vue, copying their code directly to their page components
    this, along with other recent commits and at least one upcoming commit, aims to clean up the components folder from ones that are basically page components at the wrong folder

--- frontend/src/pages/Browse.vue
+++ frontend/src/pages/Browse.vue
@@ -1,11 +1,37 @@
 <template>
 	<main>
-		<summariesBrowser />
+		<summariesSearch
+			id="summariesSearch"
+			:fetchAllOnMount="true"
+			@fetchedSummaries="(response:PostSummary[]) => {summariesArray = response}"
+		/>
+		<div>
+			<summariesContainer :summariesArray="summariesArray" />
+		</div>
 	</main>
 </template>
 
 <script setup lang="ts">
-	import summariesBrowser from "../components/posts/browse/summariesBrowser.vue";
+	import {ref} from "vue";
+	import {PostSummary} from "../../../shared/objects/post";
+	import summariesContainer from "../components/posts/browse/summariesContainer.vue";
+	import summariesSearch from "../components/posts/browse/summariesSearch.vue";
+	const summariesArray = ref<PostSummary[]>([]);
 </script>
 
-<style scoped></style>
+<style scoped>
+	main {
+		width: clamp(10em, 90vw, 45em);
+		margin: auto;
+		padding: 0.5em;
+	}
+
+	div {
+		padding-inline: 0.5em;
+	}
+
+	#summariesSearch {
+		font-size: 1.5em;
+		border-bottom: 0.1em solid var(--textColor);
+	}
+</style>
