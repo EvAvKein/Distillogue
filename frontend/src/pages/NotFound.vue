817f2213121f0d528d1faf664ff8dd83dad0e7f4
Eve_Aviv <evavkein@gmail.com>
2023-04-14 23:22:07 +0300

    feat (NotFound.vue): change button target from home page to previous
    (and rephrased button text "return to previous page" to "back to previous page", feels less like an order)

--- frontend/src/pages/NotFound.vue
+++ frontend/src/pages/NotFound.vue
@@ -1,11 +1,14 @@
 <template>
 	<main>
 		<h3>Page Not Found!</h3>
-		<router-link to="/" class="core_borderButton">Return to Home page</router-link>
+		<button @click="() => router.go(-1)" class="core_borderButton">Back to previous page</button>
 	</main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import {useRouter} from "vue-router";
+	const router = useRouter();
+</script>
 
 <style scoped>
 	main {
