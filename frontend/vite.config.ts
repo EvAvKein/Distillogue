fae8b4ddab936d265df11006307ffc36466991f8
Eve_Aviv <evavkein@gmail.com>
2023-04-17 21:14:23 +0300

    fix (dockerized dev setup): make vite recompile on file updates
    and with this commit, issue #1 is resolved

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,10 +6,6 @@ export default defineConfig({
 	plugins: [vue()],
 	server: {
 		port: 3000,
-		watch: {
-			usePolling: true,
-			/* ^ fixes dockerized file-watching on windows, can be safely disabled on linux for reduced CPU usage. info: https://vitejs.dev/config/server-options.html#server-watch, https://github.com/microsoft/WSL/issues/4739 */
-		},
 	},
 	define: {
 		"process.env": {},
