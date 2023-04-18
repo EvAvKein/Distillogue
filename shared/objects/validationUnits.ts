59a4b06c7e4714ed9c4e02b919abd969f6c66379
Eve_Aviv <evavkein@gmail.com>
2023-04-18 20:11:39 +0300

    docs (validationUnits.ts): delete code-comment
    looked into it and doesn't seem possible, too verbose to be worth keeping as a comment imo

--- shared/objects/validationUnits.ts
+++ shared/objects/validationUnits.ts
@@ -24,15 +24,3 @@ const node = {
 } as const;
 
 export {user, node};
-
-// there doesn't seem to be a way to assign a type/interface like the one below (for auto-completion), while simultaneously typing the same object "as const" so the object's properties would not be typed as unions when imported
-// if there actually is a way, pull request is appreciated as usual
-//
-// type unitsObj = {
-// 	readonly [key: string]:
-// 		| {
-// 				min?: number;
-// 				max?: number;
-// 		  }
-// 		| unitsObj;
-// }
