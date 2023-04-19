7e56824c3ddd4624d8a0b58839f78ba8ab8ff2e2
Eve_Aviv <evavkein@gmail.com>
2023-04-19 14:27:42 +0300

    build: docker-related and misc adjustments (see desc)
    - explicitly labelled cert (i.e cerbot/certification, HTTPS) files and removed undue app deployment from the process
    - removed unnecessary nginx dockerfiles
    - switched containerized dev setup's port from 80 to 3000 (may or may not add env in future to modify those)
    - turned base docker-compose.yaml file into docker-compose.prod.yaml, deleting docker-compose.httpsOverride.yaml and running CI on docker-compose.dev.yaml (see new TODO task regarding running tests on dev setup)
    - simplified README execution instructions (included in this commit because, with the removal of plain/stable docker-compose up, the commands table is too short to justify the format... and because i'm soon planning on removing non-docker support/instructions entirely)

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 docker-compose down &&
-docker-compose up --detach --build --force-recreate &&
-docker-compose -f docker-compose.ssl.yaml up --build --force-recreate &&
+docker-compose -f docker-compose.cert.yaml up --build --force-recreate &&
 docker-compose down &&
-docker-compose -f docker-compose.yaml -f docker-compose.httpsOverride.yaml up --build --force-recreate
+docker-compose -f docker-compose.prod.yaml up --build --force-recreate
--- testing/playwright.config.ts
+++ testing/playwright.config.ts
@@ -7,7 +7,7 @@ const config: PlaywrightTestConfig = {
 	expect: {timeout: 5 * 1000},
 
 	use: {
-		baseURL: `http://localhost:${process.env.DOCKERIZED ? 80 : 3000}`,
+		baseURL: "http://localhost:3000",
 		trace: "on",
 		// launchOptions: {slowMo: 500},
 	},
