12f7bef1e377348eac31c9a10e913842608b2d0b
Eve_Aviv <evavkein@gmail.com>
2023-04-19 22:37:32 +0300

    refactor (docker): rename httpsOverride to prodOverride
    fits better alongside devOverride

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ docker-compose down &&
 docker-compose up --detach --build --force-recreate &&
 docker-compose -f compose.ssl.yaml up --build --force-recreate &&
 docker-compose down &&
-docker-compose -f compose.yaml -f compose.httpsOverride.yaml up --build --force-recreate
+docker-compose -f compose.yaml -f compose.prodOverride.yaml up --build --force-recreate
