53564bc94713829066a2cd6890effd754391eb80
Eve_Aviv <evavkein@gmail.com>
2023-04-21 23:29:06 +0300

    refactor (animatedCollapsible.vue): add optional "forcedState" override
    to be used in an upcoming commit

--- frontend/src/components/animatedCollapsible.vue
+++ frontend/src/components/animatedCollapsible.vue
@@ -1,9 +1,9 @@
 <template>
 	<section :aria-expanded="open">
-		<button class="core_contentButton" type="button" @click="open = !open">
+		<button class="core_contentButton" type="button" @click="forcedState?.state ?? (open = !open)">
 			<slot name="summary"></slot>
 		</button>
-		<transition name="collapse" v-show="open">
+		<transition name="collapse" v-show="forcedState?.state ?? open">
 			<slot name="content"></slot>
 		</transition>
 	</section>
@@ -14,6 +14,7 @@
 
 	const props = defineProps<{
 		openByDefault?: true;
+		forcedState?: {state: boolean}; // workaround to an 'unintuitive' vue ruling: https://github.com/vuejs/vue/issues/4792#issue-203322901
 	}>();
 
 	const open = ref(props.openByDefault || false);
