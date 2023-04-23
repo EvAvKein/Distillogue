612f0c09b6ec3fee530d85bd5f48533bb6a8d752
Eve_Aviv <evavkein@gmail.com>
2023-04-24 01:24:04 +0300

    refactor (animatedCollapsible.vue): readability pass

--- frontend/src/components/animatedCollapsible.vue
+++ frontend/src/components/animatedCollapsible.vue
@@ -3,21 +3,22 @@
 		<button class="core_contentButton" type="button" @click="clicked">
 			<slot name="summary"></slot>
 		</button>
-		<transition name="collapse" v-show="forcedState?.state ?? open">
+		<transition name="collapse" v-show="open">
 			<slot name="content"></slot>
 		</transition>
 	</section>
 </template>
 
 <script setup lang="ts">
-	import {ref} from "vue";
+	import {ref, computed} from "vue";
 
 	const props = defineProps<{
 		openByDefault?: true;
 		forcedState?: {state: boolean}; // workaround to an 'unintuitive' vue ruling: https://github.com/vuejs/vue/issues/4792#issue-203322901
 	}>();
 
-	const open = ref(props.openByDefault || false);
+	const toggled = ref(props.openByDefault || false);
+	const open = computed(() => props.forcedState?.state ?? toggled.value);
 
 	const emit = defineEmits(["toggle"]);
 	function clicked() {
@@ -26,7 +27,7 @@
 		// assigning the toggling function to a click event on the slotted elem isn't compatible with keyboard interactions, as the focus is on this button when the handler is on the slotted elem.
 		// emitting this event allows the parent component to assign the handler to the toggle event instead (and this doesn't require an extra keyboard handler on this component's button because keyboard interactions on button trigger the click event)
 
-		props.forcedState?.state ?? (open.value = !open.value);
+		if (!props.forcedState?.state) toggled.value = !toggled.value;
 	}
 </script>
 
