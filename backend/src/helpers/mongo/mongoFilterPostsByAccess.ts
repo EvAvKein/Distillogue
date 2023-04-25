ff3f379776e790d2bf327edca46ae82fe65c813d
Eve_Aviv <evavkein@gmail.com>
2023-04-25 09:50:58 +0300

    docs (mongoFilterPostsByAccess.ts): add code comment & TODO regarding type issue

--- backend/src/helpers/mongo/mongoFilterPostsByAccess.ts
+++ backend/src/helpers/mongo/mongoFilterPostsByAccess.ts
@@ -7,3 +7,6 @@ export function mongoFilterPostsByAccess(userId: UserData["id"] | null | undefin
 		$and: [{$or: [{"access.public": true}, {"access.users.id": userId}]}, mongoFilterObject ?? {}],
 	};
 }
+
+// regarding why it causes type errors: https://github.com/mongodb/node-mongodb-native/blob/HEAD/etc/notes/CHANGES_5.0.0.md#dot-notation-typescript-support-removed-by-default
+// TODO: look for workaround
