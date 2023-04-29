82603fd6f4761e3263cbe667fa2d6196816a235e
Eve_Aviv <evavkein@gmail.com>
2023-04-29 12:21:51 +0300

    test: create template file
    will make it easier to get the boilerplate syntax out of the way, rather than manually typing it or copy-pasting and deleting stuff

---
+++ testing/tests/_template.ts
@@ -0,0 +1,6 @@
+import {test, expect, type Page} from "@playwright/test";
+import {} from "../helpers/requestsByApi.js";
+
+test.describe("", async () => {
+	test("", async ({request, page}) => {});
+});
