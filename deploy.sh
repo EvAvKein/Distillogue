f66433dc391042c4d82f69919c85cf5afd6e55c4
Eve_Aviv <evavkein@gmail.com>
2023-04-30 10:47:55 +0300

    fix (deploy.sh): create admins.json if doesn't already exist
    will prevent the docker-compose volume from creating it as a folder (which turns out is what happens when the volumed file doesn't exist), and remove the chore of creating it manually on a fresh deployment machine/VM

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ docker-compose down &&
 docker-compose -f compose.cert.yaml up --build --force-recreate &&
 docker-compose down &&
 echo "PROD=TRUE" > ./prod.env && # sufficient for now as it's (as of writing) only used to inform backend whether to create test endpoints, it'll obviously be replaced when actual confidential data is used
+(cat backend/admin/admins.json || echo "[]" > backend/admin/admins.json) &&
 docker-compose -f compose.yaml -f compose.prodOverride.yaml up --build --force-recreate
