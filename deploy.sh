84c836d810a6db909f1684866d5a854a02cd6de6
Eve_Aviv <evavkein@gmail.com>
2023-05-24 23:51:23 +0300

    fix (deploy.sh): close nginx-cert containers based on certbot container
    no clue how this shell script worked fine until now, but the nginx-cert container no longer auto-closes (and this commit is presumably the fix)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker-compose down &&
-docker-compose -f compose.cert.yaml up --build --force-recreate &&
+docker-compose -f compose.cert.yaml up --build --force-recreate --exit-code-from certbot &&
 docker-compose down &&
 echo "PROD=TRUE" > ./prod.env && # sufficient for now as it's (as of writing) only used to inform backend whether to create test endpoints, it'll obviously be replaced when actual confidential data is used
 (cat backend/admin/admins.json || echo "[]" > backend/admin/admins.json) &&
