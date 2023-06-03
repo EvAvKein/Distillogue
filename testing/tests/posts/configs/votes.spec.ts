ca2465cf8d3a0ad68482283171c79d8343a4fe24
Eve_Aviv <evavkein@gmail.com>
2023-06-03 11:30:38 +0300

    test (votes.spec.ts): move basic declarations outside of test callback

--- testing/tests/posts/configs/votes.spec.ts
+++ testing/tests/posts/configs/votes.spec.ts
@@ -1,26 +1,24 @@
 import {test, expect, type Page} from "@playwright/test";
 import {setupUserWithPostAndOpen} from "../../../helpers/requestsByUi.js";
 
+const upvoteSelector = 'button[aria-label="Upvote"]';
+const voteNumbSelector = 'span[aria-label="Votes status"]';
+const downvoteSelector = 'button[aria-label="Downvote"]';
+
+async function validateVoteInterface(page: Page, userVote: "up" | "down" | "none", voteCount: number) {
+	const upvoteElem = page.locator(upvoteSelector);
+	const countElem = page.locator(voteNumbSelector);
+	const downvoteElem = page.locator(downvoteSelector);
+
+	// if there's some way to just make the "not" bit conditional, that'd be great
+	userVote === "up" ? await expect(upvoteElem).toHaveClass(/voted/) : await expect(upvoteElem).not.toHaveClass(/voted/);
+	userVote === "down"
+		? await expect(downvoteElem).toHaveClass(/voted/)
+		: await expect(downvoteElem).not.toHaveClass(/voted/);
+	await expect(countElem).toHaveText(String(voteCount));
+}
+
 test.describe("Votes", async () => {
-	const upvoteSelector = 'button[aria-label="Upvote"]';
-	const voteNumbSelector = 'span[aria-label="Votes status"]';
-	const downvoteSelector = 'button[aria-label="Downvote"]';
-
-	async function validateVoteInterface(page: Page, userVote: "up" | "down" | "none", voteCount: number) {
-		const upvoteElem = page.locator(upvoteSelector);
-		const countElem = page.locator(voteNumbSelector);
-		const downvoteElem = page.locator(downvoteSelector);
-
-		// if there's some way to just make the "not" bit conditional, that'd be great
-		userVote === "up"
-			? await expect(upvoteElem).toHaveClass(/voted/)
-			: await expect(upvoteElem).not.toHaveClass(/voted/);
-		userVote === "down"
-			? await expect(downvoteElem).toHaveClass(/voted/)
-			: await expect(downvoteElem).not.toHaveClass(/voted/);
-		await expect(countElem).toHaveText(String(voteCount));
-	}
-
 	test.beforeEach(async ({page, request}) => {
 		setupUserWithPostAndOpen(page, request, {
 			votes: {up: true, down: true, anon: true},
