d83a6e1d7a8757774f4f3b09ea8144ec536f4e59
Eve Aviv Keinan <evavkein@gmail.com>
2023-06-17 19:48:22 +0300

    test: refactor votes.spec.ts (see desc)
    for supporting an expansion of the spec for testing absence and anonymous voting

--- testing/tests/posts/configs/votes.spec.ts
+++ testing/tests/posts/configs/votes.spec.ts
@@ -5,47 +5,59 @@ const upvoteSelector = 'button[aria-label="Upvote"]';
 const voteNumbSelector = 'span[aria-label="Votes status"]';
 const downvoteSelector = 'button[aria-label="Downvote"]';
 
-async function validateVoteInterface(page: Page, userVote: "up" | "down" | "none", voteCount: number) {
-	const upvoteElem = page.locator(upvoteSelector);
-	const countElem = page.locator(voteNumbSelector);
-	const downvoteElem = page.locator(downvoteSelector);
+async function validateVotesUI(
+	page: Page,
+	vote: {enabled: "up" | "down" | "both"; voted: "up" | "down" | "none"},
+	voteCount: number
+) {
+	const elems = {
+		up: page.locator(upvoteSelector),
+		count: page.locator(voteNumbSelector),
+		down: page.locator(downvoteSelector),
+	} as const;
 
-	// if there's some way to just make the "not" bit conditional, that'd be great
-	userVote === "up" ? await expect(upvoteElem).toHaveClass(/voted/) : await expect(upvoteElem).not.toHaveClass(/voted/);
-	userVote === "down"
-		? await expect(downvoteElem).toHaveClass(/voted/)
-		: await expect(downvoteElem).not.toHaveClass(/voted/);
-	await expect(countElem).toHaveText(String(voteCount));
+	await expect(elems.count).toHaveText(String(voteCount));
+
+	for (const voteDirection of ["up", "down"] as const) {
+		if (vote.enabled !== "both" && vote.enabled !== voteDirection) {
+			await expect(elems[voteDirection]).not.toBeVisible();
+		}
+
+		voteDirection === vote.voted
+			? await expect(elems[voteDirection]).toHaveClass(/voted/)
+			: await expect(elems[voteDirection]).not.toHaveClass(/voted/);
+		// if there's some way to just make the "not" bit conditional (without eval), that'd be nice
+	}
 }
 
 test.describe("Votes", async () => {
 	test.beforeEach(async ({page, request}) => {
-		setupUserWithPostAndOpen(page, request, {
-			votes: {up: true, down: true, anon: true},
+		await setupUserWithPostAndOpen(page, request, {
+			votes: {up: true, down: true},
 		});
-		await validateVoteInterface(page, "none", 0);
+		await validateVotesUI(page, {enabled: "both", voted: "none"}, 0);
 	});
 
 	test("Vote", async ({page}) => {
 		await page.locator(upvoteSelector).click();
-		await validateVoteInterface(page, "up", 1);
+		await validateVotesUI(page, {enabled: "both", voted: "up"}, 1);
 		await page.reload();
-		await validateVoteInterface(page, "up", 1);
+		await validateVotesUI(page, {enabled: "both", voted: "up"}, 1);
 	});
 
 	test("Override vote with opposite", async ({page}) => {
 		await page.locator(upvoteSelector).click();
 		await page.locator(downvoteSelector).click();
-		await validateVoteInterface(page, "down", -1);
+		await validateVotesUI(page, {enabled: "both", voted: "down"}, -1);
 		await page.reload();
-		await validateVoteInterface(page, "down", -1);
+		await validateVotesUI(page, {enabled: "both", voted: "down"}, -1);
 	});
 
 	test("Cancel vote", async ({page}) => {
 		await page.locator(downvoteSelector).click();
 		await page.locator(downvoteSelector).click();
-		await validateVoteInterface(page, "none", 0);
+		await validateVotesUI(page, {enabled: "both", voted: "none"}, 0);
 		await page.reload();
-		await validateVoteInterface(page, "none", 0);
+		await validateVotesUI(page, {enabled: "both", voted: "none"}, 0);
 	});
 });
