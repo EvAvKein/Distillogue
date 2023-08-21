8f678148c8f8434bd95f9097cff8325eb0cbfac1
Eve Aviv Keinan <evavkein@gmail.com>
2023-08-21 10:08:17 +0300

    refactor (frontend - helpers): export type and return timer in debounce.ts
    for external timer clearouts (used in upcoming commit)

--- frontend/src/helpers/debounce.ts
+++ frontend/src/helpers/debounce.ts
@@ -1,5 +1,7 @@
-let timer: undefined | ReturnType<typeof setTimeout>;
+export type timerId = undefined | ReturnType<typeof setTimeout>; // using ReturnType because NodeJS uses a different value/type from the web
+
+let timer: timerId;
 export function debounce(delay: number, func: () => void) {
 	clearTimeout(timer);
-	timer = setTimeout(func, delay);
+	return (timer = setTimeout(func, delay));
 }
