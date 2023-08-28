2b5eda83c9640128e1ab76ec8422f99c5aeac9de
Eve Aviv Keinan <evavkein@gmail.com>
2023-08-28 12:29:01 +0300

    refactor: adjust notification.vue colors (see desc)
    - removed transparency from positive & negative backgrounds (it was an airheaded way to achieve the colors i wanted, and now that i'm about to reuse those colors in overlay notifications the transparency became an issue)
    - minor changes to neutral notif colors (purely for hex readability, visual effect is negligible)

--- frontend/src/components/notification.vue
+++ frontend/src/components/notification.vue
@@ -46,17 +46,17 @@
 		font-size: 1em;
 		padding: 0.3em;
 		color: var(--textColor);
-		background-color: #797979;
-		border-inline: 0.2em solid #505050;
+		background-color: #666666;
+		border-inline: 0.2em solid #888888;
 		margin: 0.75em auto 0.75em;
 	}
 	.notification.negative {
 		border-color: #ff0000;
-		background-color: #ff000088;
+		background-color: #880000;
 	}
 	.notification.positive {
 		border-color: #00ff00;
-		background-color: #00ff0088;
+		background-color: #008800;
 	}
 
 	.notification-enter-from {
