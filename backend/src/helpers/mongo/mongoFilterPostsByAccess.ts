5ac634054ef591857490aa4b8bbdba76db17dba0
Eve Aviv Keinan <evavkein@gmail.com>
2023-10-23 18:12:50 +0300

    refactor (backend - helpers): add role-requirement option in mongoFilterPostsByAccess.ts

--- backend/src/helpers/mongo/mongoFilterPostsByAccess.ts
+++ backend/src/helpers/mongo/mongoFilterPostsByAccess.ts
@@ -1,10 +1,17 @@
 import {UserData} from "../../../objects/user";
 import {Post} from "../../../objects/post";
 import {Filter} from "mongodb";
+import {type PostUserRole} from "../../../objects/user";
 
-export function mongoFilterPostsByAccess(userData: UserData | null | undefined, mongoFilterObject?: Filter<Post>) {
+export function mongoFilterPostsByAccess(
+	userData: UserData | null | undefined,
+	mongoFilterObject?: Filter<Post>,
+	roleRequired?: PostUserRole
+) {
 	const permissionsFilter = userData?.permissions.admin
 		? {} // might in future be narrowed to posts that were reported, if userbase prefers (and laws permit)
+		: roleRequired
+		? {"access.users": {$elemMatch: {id: userData?.id, roles: roleRequired}}}
 		: {"access.users.id": userData?.id};
 
 	return {
